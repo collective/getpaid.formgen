549de25e8944cfe8f7d05e746ff9c3eb3f41562f
Alec Mitchell <alecpm@gmail.com>
2013-10-07 23:40:19 -0700

    Remove unneeded and deprecated imports.  Use new getpaid.hurry.workflow package path for Plone 4.3 compatibility.

--- src/getpaid/formgen/interfaces.py
+++ src/getpaid/formgen/interfaces.py
@@ -1,10 +1,5 @@
-from zope import schema
 from zope.interface import Interface
 
-from zope.app.container.constraints import contains
-from zope.app.container.constraints import containers
-
-from getpaid.formgen import GPFGMessageFactory as _
 
 class IMakePaymentProcess(Interface):
     """
